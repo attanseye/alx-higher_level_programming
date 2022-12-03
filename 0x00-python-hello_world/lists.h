0adba807bb9a3f91b8c384cf2c99134bdb0ab336
attanseye <attanseye@gmail.com>
2022-12-03 04:37:09 -0800

    task 1 corrections

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,10 +3,17 @@
 
 #include <stdlib.h>
 
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-		int n;
+	int n;
 			struct listint_s *next;
 } listint_t;
 
